af52008b968de433d90588cd2baf9177e7c7054f
andreas19 <andreas19@posteo.eu>
2023-03-31 12:21:31 +0200

    Add Map2() and Map3()

--- map.go
+++ map.go
@@ -3,7 +3,7 @@ package slices
 // A MapFunc maps a value to another value which may have a different type.
 type MapFunc[T, U any] func(x T) U
 
-// Map maps a values in a slice using a mapping function.
+// Map maps values in a slice using a mapping function.
 func Map[T, U any](sl []T, f MapFunc[T, U]) []U {
 	if sl == nil {
 		return nil
@@ -11,6 +11,40 @@ func Map[T, U any](sl []T, f MapFunc[T, U]) []U {
 	result := make([]U, len(sl))
 	for i, v := range sl {
 		result[i] = f(v)
+	}
+	return result
+}
+
+// A Map2Func maps two values to another value.
+type Map2Func[T, U, V any] func(x T, y U) V
+
+// Map2 maps values in two slices using a mapping function.
+// It stops when the shortest slice is exhaustet.
+func Map2[T, U, V any](sl1 []T, sl2 []U, f Map2Func[T, U, V]) []V {
+	if sl1 == nil || sl2 == nil {
+		return nil
+	}
+	length := Min([]int{len(sl1), len(sl2)})
+	result := make([]V, length)
+	for i := 0; i < length; i++ {
+		result[i] = f(sl1[i], sl2[i])
+	}
+	return result
+}
+
+// A Map2Func maps three values to another value.
+type Map3Func[T, U, V, W any] func(x T, y U, z V) W
+
+// Map3 maps values in three slices using a mapping function.
+// It stops when the shortest slice is exhaustet.
+func Map3[T, U, V, W any](sl1 []T, sl2 []U, sl3 []V, f Map3Func[T, U, V, W]) []W {
+	if sl1 == nil || sl2 == nil || sl3 == nil {
+		return nil
+	}
+	length := Min([]int{len(sl1), len(sl2), len(sl3)})
+	result := make([]W, length)
+	for i := 0; i < length; i++ {
+		result[i] = f(sl1[i], sl2[i], sl3[i])
 	}
 	return result
 }
--- map_test.go
+++ map_test.go
@@ -21,3 +21,41 @@ func TestMap(t *testing.T) {
 		}
 	}
 }
+
+func TestMap2(t *testing.T) {
+	var tests = []struct {
+		sl1, sl2, want []int
+	}{
+		{nil, nil, nil},
+		{nil, []int{}, nil},
+		{[]int{}, nil, nil},
+		{[]int{}, []int{1}, []int{}},
+		{[]int{1}, []int{1}, []int{2}},
+		{[]int{1, 2}, []int{3, 4}, []int{4, 6}},
+	}
+	f := func(x, y int) int { return x + y }
+	for i, test := range tests {
+		if got := Map2(test.sl1, test.sl2, f); !reflect.DeepEqual(got, test.want) {
+			t.Errorf("%d: got %#v, want %#v", i, got, test.want)
+		}
+	}
+}
+
+func TestMap3(t *testing.T) {
+	var tests = []struct {
+		sl1, sl2, sl3, want []int
+	}{
+		{nil, nil, nil, nil},
+		{nil, []int{}, []int{}, nil},
+		{[]int{}, nil, []int{}, nil},
+		{[]int{}, []int{1}, []int{}, []int{}},
+		{[]int{1}, []int{1}, []int{1}, []int{3}},
+		{[]int{1, 2}, []int{3, 4}, []int{5, 6}, []int{9, 12}},
+	}
+	f := func(x, y, z int) int { return x + y + z }
+	for i, test := range tests {
+		if got := Map3(test.sl1, test.sl2, test.sl3, f); !reflect.DeepEqual(got, test.want) {
+			t.Errorf("%d: got %#v, want %#v", i, got, test.want)
+		}
+	}
+}
