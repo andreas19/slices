4fe20aafa6a4eaea647c5bfa02f6778cbf7cc1a0
andreas19 <andreas19@posteo.eu>
2025-06-15 07:14:17 +0200

    Add Values()

--- iter.go
+++ iter.go
@@ -2,10 +2,22 @@ package slices
 
 import "iter"
 
+// Iter returns an iterator over index-values pairs in the slice.
 func Iter[T any](sl []T) iter.Seq2[int, T] {
 	return func(yield func(int, T) bool) {
 		for i, v := range sl {
 			if !yield(i, v) {
+				return
+			}
+		}
+	}
+}
+
+// Values returns an iterator over the values in the slice.
+func Values[T any](sl []T) iter.Seq[T] {
+	return func(yield func(T) bool) {
+		for _, v := range sl {
+			if !yield(v) {
 				return
 			}
 		}
